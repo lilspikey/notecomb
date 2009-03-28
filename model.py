3d070f154e56a5255cfb90915f41d209f99f89f2
John Montgomery <john@littlespikeyland.com>
2009-03-28 11:51:44 +0000

    implemented "remove" for document and got it working with search on/off.

--- model.py
+++ model.py
@@ -28,24 +28,76 @@ class Document(object):
         return self._text
     
     def insert(self, offset, text):
-        current_offset=0
+        visible_offset, visible, other_visible = self._find_visible_from_offset(offset)
+        actual_offset=visible.offset + (offset-visible_offset)
+        self._text=self._text[:actual_offset]+text+self._text[actual_offset:]
+        length=len(text)
+        visible.length += length
+        # shift other visible sections along by same amount
+        self._move_visible(other_visible,length)
+    
+    def _find_visible_from_offset(self,offset):
+        visible_offset=0
         for i, visible in enumerate(self._visible):
-            if current_offset <= offset <= current_offset+visible.length:
-                actual_offset=visible.offset + (offset-current_offset)
-                self._text=self._text[:actual_offset]+text+self._text[actual_offset:]
-                length=len(text)
-                visible.length += length
-                # shift other visible sections along by same amount
-                self._move_visible(self._visible[i+1:],length)
-                break
-            current_offset += visible.length
+            if visible_offset <= offset <= visible_offset+visible.length:
+                return visible_offset, visible, self._visible[i+1:]
+            visible_offset += visible.length
+        raise ValueError("invalid offset %d" % offset)
     
     def _move_visible(self,visible,length):
         for v in visible:
             v.offset += length
     
     def remove(self, offset, length):
-        pass
+        length_removed=0
+        while length:
+            visible_offset, visible, other_visible = self._find_visible_from_offset(offset+1)
+            actual_offset=visible.offset + (offset-visible_offset)
+        
+            # how much we can remove from this visible section
+            length_removed=min(length, visible.length-(offset-visible_offset))
+        
+            self._text=self._text[:actual_offset]+self._text[actual_offset+length_removed:]
+            visible.length -= length_removed
+        
+            self._move_visible(other_visible,-length_removed)
+            
+            length -= length_removed
+        self._merge_visible()
+    
+    def _merge_visible(self):
+        # any visible lines that are now joined together,
+        # should be joined together in the real underlying text
+        while True:
+            merged=False
+            for i, visible in enumerate(self._visible[:-1]):
+                text=visible.visible(self._text)
+                if not text.endswith("\n"):
+                    merged=True
+                    other_visible=self._visible[i+1]
+                    other_text=other_visible.visible(self._text)
+                    
+                    self._move_text(other_visible.offset, visible.offset+visible.length, other_text)
+                    
+                    self._visible.remove(other_visible)
+                    
+                    visible.length += len(other_text)
+                    self._move_visible(self._visible[i+1:], len(other_text))
+                    
+                    break
+            if not merged:
+                # make sure there's a newline after the last visible section (if there's more text after it)
+                last_visible=self._visible[-1]
+                text=last_visible.visible(self._text)
+                if not text.endswith("\n"):
+                    last_offset=last_visible.offset+last_visible.length
+                    # check more text after visiable and not empty
+                    if last_offset < len(self._text) and not last_visible.is_empty:
+                        self._text = self._text[:last_offset] + "\n" + self._text[last_offset:]
+                return
+    
+    def _move_text(self,from_offset,to_offset,text):
+        self._text=self._text[:to_offset]+text+self._text[to_offset:from_offset]+self._text[from_offset+len(text):]
     
     class Visible(object):
         def __init__(self,offset,length):
@@ -55,3 +107,6 @@ class Document(object):
         def visible(self,text):
             return text[self.offset:self.offset+self.length]
         
+        @property
+        def is_empty(self):
+            return self.length == 0
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 from model import Document
 
@@ -9,15 +10,33 @@ class DocumentTestCase(unittest.TestCase):
 
     def test_insert(self):
         self.doc.insert(0,'here')
-        self.assert_(self.doc.visible_text, 'here')
+        self.assertEqual(self.doc.visible_text, 'here')
+        
         self.doc.insert(0,'again ')
         self.assertEqual(self.doc.visible_text, 'again here')
+        
         self.doc.insert(8,'1')
         self.assertEqual(self.doc.visible_text, 'again he1re')
+        
         self.doc.insert(8,'23')
         self.assertEqual(self.doc.visible_text, 'again he231re')
+        
         self.doc.insert(len(self.doc.visible_text),'ing')
         self.assertEqual(self.doc.visible_text, 'again he231reing')
+    
+    def test_remove(self):
+        self.doc.insert(0,'here is some stuff\nover a few lines')
+        self.assertEqual(self.doc.visible_text, 'here is some stuff\nover a few lines')
+        
+        self.doc.remove(0,5)
+        self.assertEqual(self.doc.visible_text, 'is some stuff\nover a few lines')
+        
+        self.doc.remove(1,1)
+        self.assertEqual(self.doc.visible_text, 'i some stuff\nover a few lines')
+        
+        self.doc.remove(len(self.doc.visible_text)-4,4)
+        self.assertEqual(self.doc.visible_text, 'i some stuff\nover a few l')
+    
     
     def test_search(self):
         self.doc.insert(0,'hello there\nthis is a test\nof search')
@@ -59,4 +78,68 @@ class DocumentTestCase(unittest.TestCase):
         self.doc.insert(15, '23')
         self.assertEqual(self.doc.visible_text,'hello therego\nh23ello of search')
         self.assertEqual(self.doc.text,'hello therego\nagain this is a test\nh23ello of search')
+    
+    def test_search_and_remove(self):
+        self.doc.search('')
+        self.doc.insert(0,'hello there\nthis is a test\nof search\nhello there')
         
+        self.doc.search('hello')
+        self.assertEqual(self.doc.visible_text, 'hello there\nhello there')
+        
+        self.doc.remove(0, 3)
+        self.assertEqual(self.doc.visible_text, 'lo there\nhello there')
+        
+        self.doc.remove(len(self.doc.visible_text)-3, 3)
+        self.assertEqual(self.doc.visible_text, 'lo there\nhello th')
+        
+        # now remove accross multiple visible regions
+        self.doc.remove(7, 3)
+        self.assertEqual(self.doc.visible_text, 'lo therello th')
+        
+        # finally check underlying text matches what we expect
+        self.assertEqual(self.doc.text, 'lo therello th\nthis is a test\nof search\n')
+    
+    def test_insert_remove(self):
+        # in the simple case (without search) inserting them remove should
+        # have no effect
+        self.doc.insert(0,'hello there this is a test\n of inserting and removing\nsome text')
+        self._check_inserting_and_removing()
+    
+    def test_insert_remove_while_searching(self):
+        # when inserting and removing (in that order) with search nothing should change either
+        self.doc.insert(0,'hello there this is a test\n of inserting and removing\nsome text')
+        
+        self.doc.search('te')
+        self._check_inserting_and_removing()
+        
+        self.doc.search('hello')
+        self._check_inserting_and_removing()
+        
+        self.doc.search('inserting')
+        self._check_inserting_and_removing()
+        
+        self.doc.search('some')
+        self._check_inserting_and_removing()
+        
+        # now try a search with no visible lines
+        self.doc.search('876fdjfdsf87')
+        self._check_inserting_and_removing()
+    
+    def _check_inserting_and_removing(self):
+        for i in range(0,200):
+            text_length=len(self.doc.visible_text)
+            offset=random.choice(range(text_length+1))
+            length=random.choice(range(text_length+1)) + 1
+            
+            s=''.join(random.choice('gsgs8hkjefw98y') for i in range(length))
+            
+            visible_text_before=self.doc.visible_text
+            text_before=self.doc.text
+            
+            self.doc.insert(offset, s)
+            self.assert_( visible_text_before != self.doc.visible_text )
+            self.assert_( text_before != self.doc.text )
+            
+            self.doc.remove(offset, length)
+            self.assertEqual( self.doc.visible_text, visible_text_before )
+            self.assertEqual( self.doc.text, text_before )
