c95b312a22574120940de4a399b0077702f0114d
John Montgomery <john@littlespikeyland.com>
2009-03-28 14:26:36 +0000

    initial document hooked up to ui.

--- main.py
+++ main.py
@@ -2,34 +2,79 @@ import wx
 import wx.stc
 import sys
 
+from model import Document
+
 class MainFrame(wx.Frame):
     def __init__(self,parent):
         wx.Frame.__init__(self,parent,title='Observertron')
         
+        
+        self.doc=Document()
+        
         self.menubar=wx.MenuBar()
+        
+        self.search=wx.SearchCtrl(self)
+        self.search.ShowCancelButton(True)
         
         self.text=wx.stc.StyledTextCtrl(self,style=wx.TE_MULTILINE|wx.NO_BORDER|wx.WANTS_CHARS)
         
-        self.text.SetText('\n'.join(["one","two","three","four"]))
-        #self.text.SetFoldFlags(64)
-        #self.text.HideLines(0,1)
-        
         self.Bind(wx.stc.EVT_STC_MODIFIED, self.Modified)
+        self.Bind(wx.EVT_TEXT, self.Search)
+        self.Bind(wx.EVT_SEARCHCTRL_CANCEL_BTN, self.SearchCancelled)
         
         sizer=wx.BoxSizer(wx.VERTICAL)
         
-        sizer.Add(self.text, 1, flag=wx.EXPAND)
+        sizer.Add(self.search, 0, flag=wx.EXPAND)
+        sizer.AddSpacer((5,5))
+        sizer.Add(self.text, 1, flag=wx.EXPAND|wx.GROW|wx.ALL)
         
         self.SetAutoLayout(True)
         self.SetSizer(sizer)
         self.Layout()
     
+    def SearchCancelled(self,event):
+        self.search.SetValue('')
+    
+    def Search(self,event):
+        q=self.search.GetValue()
+        self.doc.search(q)
+        self._update_visible_text()
+    
+    def _update_visible_text(self):
+        mod_mask=self.text.GetModEventMask()
+        self.text.SetModEventMask(0)
+        self.text.SetText(self.doc.visible_text)
+        self.text.SetModEventMask(mod_mask)
+    
     def Modified(self,event):
-        print "event.GetModificationType()", event.GetModificationType()
-        print "event.GetLine()", event.GetLine()
-        print "event.GetPosition()", event.GetPosition()
-        print "event.GetLinesAdded()", event.GetLinesAdded()
-        print "event.GetText()", event.GetText()
+        mod_type=event.GetModificationType()
+        action=None
+        
+        if mod_type & wx.stc.STC_MOD_INSERTTEXT:
+            action=self.ModifiedInsertText
+        elif mod_type & wx.stc.STC_MOD_DELETETEXT:
+            action=self.ModifiedDeleteText
+        
+        if action:
+            action(event)
+        
+        print mod_type, wx.stc.STC_MOD_INSERTTEXT
+        print repr(self.doc.visible_text)
+        print repr(self.text.GetText())
+    
+    def ModifiedInsertText(self, event):
+        if self.doc.visible_text != self.text.GetText():
+            offset=event.GetPosition()
+            text=event.GetText()
+        
+            self.doc.insert(offset, text)
+    
+    def ModifiedDeleteText(self, event):
+        if self.doc.visible_text != self.text.GetText():
+            offset=event.GetPosition()
+            length=event.GetLength()
+        
+            self.doc.remove(offset, length)
 
 class App(wx.App):
 
