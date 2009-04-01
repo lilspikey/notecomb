9daf131947409bf75b44d106a4cf12d1fcd39a16
John Montgomery <john@littlespikeyland.com>
2009-04-01 13:54:23 +0100

    updated setup script to only have one call to setup. also added manifest for py2exe.

--- setup.py
+++ setup.py
@@ -1,46 +1,76 @@
 from distutils.core import setup
 
-def setup_py2app():
-    import py2app
-    
-    plist = {
-        'CFBundleName':               "Observertron",
-        'CFBundleShortVersionString': "0.2.0",
-        'CFBundleGetInfoString':      "Observertron 0.2.0 alpha",
-        'CFBundleExecutable':         "Observertron",
-        'CFBundleIdentifier':         "com.psychicorigami.observertron",
-        'CFBundleDocumentTypes': [ { 'CFBundleTypeExtensions': ["*"], 'CFBundleTypeName': "kUTTypeText", 'CFBundleTypeRole': "Editor" } ],
-    }
-    
-    # Build the .app file
-    setup(
-        options=dict(
-            py2app=dict(
-                argv_emulation=1,
-                #iconfile='resources/myapp-icon.icns',
-                #packages='wx',
-                #site_packages=True,
-                plist=plist
-            ),
-        ),
-        app=[ 'main.py' ]
-    )
 
-
-def setup_py2exe():
-    import py2exe
-    setup(
-        windows=[dict(
-            script="main.py",
-            name="Observertron",
-            dest_base="Observertron",
-            version="0.2.0")],
-        options = {'py2exe': {'bundle_files': 1}},
-        zipfile = None,
-    )
-
-for fn in [setup_py2app, setup_py2exe]:
+def py2app_options(setup_args):
     try:
-        fn()
+        import py2app
+        # create py2app options
+        py2app_plist = {
+            'CFBundleName':               "Observertron",
+            'CFBundleShortVersionString': "0.2.0",
+            'CFBundleGetInfoString':      "Observertron 0.2.0 alpha",
+            'CFBundleExecutable':         "Observertron",
+            'CFBundleIdentifier':         "com.psychicorigami.observertron",
+            'CFBundleDocumentTypes': [ { 'CFBundleTypeExtensions': ["*"], 'CFBundleTypeName': "kUTTypeText", 'CFBundleTypeRole': "Editor" } ],
+        }
+
+        setup_args['options']['py2app']={
+            'argv_emulation': 1,
+            #'iconfile': 'resources/myapp-icon.icns',
+            'plist': py2app_plist,
+        }
+
+        setup_args['app']=[ 'main.py' ]
+        # end py2app options
     except ImportError:
         pass
+
+def py2exe_options(setup_args):
+    try:
+        import py2exe
+        # create py2exe options
+        py2exe_manifest = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+        <assembly xmlns="urn:schemas-microsoft-com:asm.v1"
+        manifestVersion="1.0">
+        <assemblyIdentity
+            version="0.64.1.0"
+            processorArchitecture="x86"
+            name="Controls"
+            type="win32"
+        />
+        <description>Observertron</description>
+        <dependency>
+            <dependentAssembly>
+                <assemblyIdentity
+                    type="win32"
+                    name="Microsoft.Windows.Common-Controls"
+                    version="6.0.0.0"
+                    processorArchitecture="X86"
+                    publicKeyToken="6595b64144ccf1df"
+                    language="*"
+                />
+            </dependentAssembly>
+        </dependency>
+        </assembly>
+        """
+
+        setup_args['windows']=[{
+            'script': "main.py",
+            'name': "Observertron",
+            'dest_base': "Observertron",
+            'version': "0.2.0",
+            "other_resources": [(24,1,py2exe_manifest)]
+        }]
+
+        setup_args['py2exe']={'bundle_files': 1}
+        setup_args['zipfile'] = None
+        # end of py2exe options
+    except ImportError:
+        pass
+
+setup_args={'options':{}}
+py2app_options(setup_args)
+py2exe_options(setup_args)
+
+# now run setup
+setup(**setup_args)
